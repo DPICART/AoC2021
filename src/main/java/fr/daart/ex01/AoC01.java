c0b8cd5afd220aea8381b76d5b7660426502dab5
Daart <david.pciart@outlook.fr>
2021-12-01 05:22:51 +0100

    aoc01: part 1 & part2

--- src/main/java/fr/daart/ex01/AoC01.java
+++ src/main/java/fr/daart/ex01/AoC01.java
@@ -2,10 +2,56 @@ package main.java.fr.daart.ex01;
 
 import main.java.fr.daart.AoC2021;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AoC01 extends AoC2021 {
 
     @Override
     public void run() {
+
+        partOne();
+        partTwo();
+
+    }
+
+
+    void partOne() {
+        var list = readInput("input.txt");
+        var previous = Integer.MAX_VALUE;
+        var increased = 0;
+        for(int i = 0; i < list.size(); i++) {
+            var current = Integer.parseInt(list.get(i));
+            if(current > previous) {
+                increased++;
+            }
+            previous = current;
+        }
+        System.out.println(increased);
+        // 1139
+    }
+
+    void partTwo() {
+        var list = readInput("input.txt");
+
+        List<Integer> newList = new ArrayList<>();
+
+        for(int i = 2; i < list.size(); i++) {
+            var newVal = Integer.parseInt(list.get(i-2)) + Integer.parseInt(list.get(i-1)) + Integer.parseInt(list.get(i));
+            newList.add(newVal);
+        }
+
+        var previous = Integer.MAX_VALUE;
+        var increased = 0;
+        for(int i = 0; i < newList.size(); i++) {
+            var current = newList.get(i);
+            if(current > previous) {
+                increased++;
+            }
+            previous = current;
+        }
+        System.out.println(increased);
+        // 1103
     }
 
 }
