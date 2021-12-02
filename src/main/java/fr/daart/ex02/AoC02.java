e3a355ff0c5be34e35e8914535b7d18790735e81
Daart <david.pciart@outlook.fr>
2021-12-02 06:14:13 +0100

    aoc02: part 1 & part2

---
+++ src/main/java/fr/daart/ex02/AoC02.java
@@ -0,0 +1,89 @@
+package main.java.fr.daart.ex02;
+
+import main.java.fr.daart.AoC2021;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class AoC02 extends AoC2021 {
+
+    @Override
+    public void run() {
+
+        partOne();
+        partTwo();
+
+    }
+
+
+    void partOne() {
+        int depth = 0;
+        int horizontal = 0;
+
+        List<String> moves = readInput("input.txt");
+
+        for (var m : moves) {
+            var tab = m.split(" ");
+            var verb = tab[0];
+            var qtt = Integer.parseInt(tab[1]);
+
+            switch (verb) {
+                case "forward":
+                    horizontal += qtt;
+                    break;
+                case "down":
+                    depth += qtt;
+                    break;
+                case "up":
+                    depth -= qtt;
+                    break;
+                case "backward":
+                    horizontal -= qtt;
+                    break;
+                default:
+                    System.out.println("Failed "+verb+" "+qtt);
+            }
+
+        }
+        System.out.println("depth "+depth+" horizontal"+horizontal+" product "+depth*horizontal);
+
+    }
+
+    void partTwo() {
+        int depth = 0;
+        int horizontal = 0;
+        int aim = 0;
+
+        List<String> moves = readInput("input.txt");
+
+        for (var m : moves) {
+            var tab = m.split(" ");
+            var verb = tab[0];
+            var qtt = Integer.parseInt(tab[1]);
+
+            switch (verb) {
+                case "forward":
+                    horizontal += qtt;
+                    depth += (aim * qtt);
+                    break;
+                case "down":
+                    //depth += qtt;
+                    aim += qtt;
+                    break;
+                case "up":
+                    //depth -= qtt;
+                    aim -= qtt;
+                    break;
+                case "backward":
+                    horizontal -= qtt;
+                    break;
+                default:
+                    System.out.println("Failed "+verb+" "+qtt);
+            }
+
+        }
+        System.out.println("depth "+depth+" horizontal"+horizontal+" product "+depth*horizontal);
+    }
+
+}
